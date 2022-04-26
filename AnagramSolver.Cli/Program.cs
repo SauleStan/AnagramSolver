876ffe518c6c916ccc8970f08173d6b5d8e760be
Saulė Stankevičiūtė <saule.stankeviciute@visma.com>
2022-04-26 10:58:45 +0300

    Anagram count constraint to output

--- AnagramSolver.Cli/Program.cs
+++ AnagramSolver.Cli/Program.cs
@@ -12,6 +12,8 @@ IConfiguration config = new ConfigurationBuilder()
 
 // Get constraints from appsettings
 var minLength = config.GetSection("Constraints").GetValue<int>("MinInput");
+var minAnagrams = config.GetSection("Constraints").GetValue<int>("MinAnagramCount");
+var maxAnagrams = config.GetSection("Constraints").GetValue<int>("MaxAnagramCount");
 
 AnagramController anagramController = new AnagramController();
 
@@ -30,8 +32,13 @@ do
 if (inputWord != null)
 {
     var words = anagramController.FindAnagrams(inputWord);
+    
     Console.WriteLine("Anagrams: ");
-    foreach (var word in words)
+    if (words.Count < minAnagrams)
+    {
+        Console.WriteLine("Less than {0} anagrams have been found. Try another word.", minAnagrams);
+    }
+    foreach (var word in words.Take(maxAnagrams))
     {
         Console.WriteLine(word);
     }
