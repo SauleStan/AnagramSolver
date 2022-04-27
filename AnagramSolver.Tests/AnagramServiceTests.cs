40520bd2bf024b3a3755657f55db5ecf8fea256f
Saulė S <saule.stankeviciute@visma.com>
2022-04-27 11:14:08 +0300

    Anagram conversion tests

--- AnagramSolver.Tests/AnagramServiceTests.cs
+++ AnagramSolver.Tests/AnagramServiceTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using AnagramSolver.BusinessLogic;
 using AnagramSolver.Contracts.Models;
 using NUnit.Framework;
@@ -37,5 +38,71 @@ public class AnagramServiceTests
 
         // Assert
         Assert.IsFalse(result);
+    }
+    
+    [Test]
+    public void ConvertToAnagram_ReturnsAnagram_WhenProvidedAString()
+    {
+        // Arrange
+        string word = "kalis";
+
+        // Act
+        var result = _anagramService.ConvertToAnagram(word);
+
+        // Assert
+        Assert.IsInstanceOf<Anagram>(result);
+    }
+    
+    [Test]
+    public void ConvertToAnagram_ReturnsAnagramWithCorrectCrib_WhenProvidedAString()
+    {
+        // Arrange
+        string word = "alus";
+        string crib = "alsu";
+
+        // Act
+        var result = _anagramService.ConvertToAnagram(word);
+
+        // Assert
+        Assert.AreEqual(result.Crib, crib);
+    }
+    
+    [Test]
+    public void ConvertToAnagrams_ReturnsAnagramHashSet_WhenProvidedStringHashSet()
+    {
+        // Arrange
+        HashSet<string> words = new HashSet<string>();
+        words.Add("alkis");
+        words.Add("alus");
+
+        // Act
+        var result = _anagramService.ConvertToAnagrams(words);
+
+        // Assert
+        Assert.IsInstanceOf<HashSet<Anagram>>(result);
+    }
+    [Test]
+    public void ConvertToAnagrams_ReturnsAnagramHashSetWithCorrectCribs_WhenProvidedStringHashSet()
+    {
+        // Arrange
+        HashSet<string> words = new HashSet<string>();
+        words.Add("alkis");
+        words.Add("alus");
+        
+        HashSet<string> cribs = new HashSet<string>();
+        cribs.Add("aikls");
+        cribs.Add("alsu");
+        
+        var resultCribs = new HashSet<string>();
+
+        // Act
+        var result = _anagramService.ConvertToAnagrams(words);
+        foreach (var anagram in result)
+        {
+            resultCribs.Add(anagram.Crib);
+        }
+
+        // Assert
+        Assert.AreEqual(cribs, resultCribs);
     }
 }
