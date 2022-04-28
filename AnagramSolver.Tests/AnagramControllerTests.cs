f8e6fa772704e713c9af56dcbc2572283e32d937
Saulė S <saule.stankeviciute@visma.com>
2022-04-28 10:45:11 +0300

    Rewritten AnagramController test

--- AnagramSolver.Tests/AnagramControllerTests.cs
+++ AnagramSolver.Tests/AnagramControllerTests.cs
@@ -1,18 +1,49 @@
+using System.Collections.Generic;
 using AnagramSolver.BusinessLogic;
+using AnagramSolver.BusinessLogic.Interfaces;
+using AnagramSolver.Contracts.Models;
 using NUnit.Framework;
+using Moq;
 
 namespace AnagramSolver.Tests;
 
 public class AnagramControllerTests
 {
+    private readonly Mock<IAnagramService> _anagramServiceMock = new ();
+    private readonly Mock<IWordService> _wordServiceMock = new ();
+    private readonly HashSet<string> _wordSetMock = new ();
+    private readonly HashSet<Anagram> _anagramSetMock = new ();
+
+    [SetUp]
+    public void SetUp()
+    {
+        Anagram anagram1 = new Anagram("alus", "alsu"); 
+        Anagram anagram2 = new Anagram("sula", "alsu"); 
+        Anagram anagram3 = new Anagram("alkis", "aikls");
+        
+        _wordSetMock.Add(anagram1.Name);
+        _wordSetMock.Add(anagram2.Name);
+        _wordSetMock.Add(anagram3.Name);
+
+        _anagramSetMock.Add(anagram1);
+        _anagramSetMock.Add(anagram2);
+        _anagramSetMock.Add(anagram3);
+
+        _wordServiceMock.Setup(x => x.GetWords()).Returns(_wordSetMock);
+        _anagramServiceMock.Setup(x => x.ConvertToAnagrams(_wordSetMock)).Returns(_anagramSetMock);
+    }
+    
     [Test]
     public void FindAnagrams_ReturnsAnagramHashSet_WhenGivenInputWord()
     {
         // Arrange
-        AnagramController anagramController = new (new AnagramService());
+        AnagramController anagramController = new (_anagramServiceMock.Object, _wordServiceMock.Object);
+        string word = "alus";
+        string crib = "alsu";
+        _anagramServiceMock.Setup(x => x.ConvertToAnagram(word)).Returns(new Anagram(word, crib));
 
         // Act
-        var result = anagramController.FindAnagrams("alus");
+        var result = anagramController.FindAnagrams(word);
         
         // Assert
         Assert.IsTrue(result.Contains("sula"));
