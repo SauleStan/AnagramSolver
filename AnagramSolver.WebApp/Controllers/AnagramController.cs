b3c7483452268a8b884655ddad46db98efca2266
Saulė S <saule.stankeviciute@visma.com>
2022-05-04 14:38:30 +0300

    Added anagram api controller

---
+++ AnagramSolver.WebApp/Controllers/AnagramController.cs
@@ -0,0 +1,22 @@
+using AnagramSolver.BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnagramSolver.WebApp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AnagramController : ControllerBase
+{
+    private readonly IAnagramResolver _anagramResolver;
+    
+    public AnagramController(IAnagramResolver anagramResolver)
+    {
+        _anagramResolver = anagramResolver;
+    }
+    
+    [HttpGet("{input}")]
+    public HashSet<string> GetAnagrams(string input)
+    {
+        return _anagramResolver.FindAnagrams(input);
+    }
+}
