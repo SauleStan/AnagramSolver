e20a37f6b09cf267e3fa602791cda7cc36e34ebd
Saulė S <saule.stankeviciute@visma.com>
2022-05-25 11:12:09 +0300

    Add routing

--- AnagramSolver.AngularApp/src/app/app.module.ts
+++ AnagramSolver.AngularApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BodyComponent } from './components/body/body.component';
@@ -9,6 +10,11 @@ import { ButtonComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
 import { WordItemComponent } from './components/word-item/word-item.component';
 import { WordsComponent } from './components/words/words.component';
+
+const appRoutes: Routes = [
+  {path: '', component: BodyComponent},
+  {path: 'words', component: WordsComponent}
+]
 
 @NgModule({
   declarations: [
@@ -22,7 +28,8 @@ import { WordsComponent } from './components/words/words.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
